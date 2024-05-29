256f2e3e688c2b0dfc2f15806a72ecd07721d3fb
FaizaFarooq23 <faizafarooq2325@gmail.com>
2024-05-29 16:24:59 +0500

    reports page issue resolved

--- src/components/Reports/Tabs.js
+++ src/components/Reports/Tabs.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import Grid from "../AdminDashboard/Grid/Grid";
 
 export default function ResearchTabs({ data }) {
+  const [ipActiveTab, setIpActiveTab] = useState(false);
+  const [activeTab, setActiveTab] = useState("Research Project Reports");
+
   const researchTabs = [
     {
       tabTitle: "Research Project Reports",
@@ -9,98 +12,91 @@ export default function ResearchTabs({ data }) {
     },
     {
       tabTitle: "Case Studies",
+      component: null,
     },
     {
       tabTitle: "Liasen Development",
+      component: null,
     },
     {
       tabTitle: "Research Links",
+      component: null,
     },
     {
       tabTitle: "Consaltancy Contracts",
+      component: null,
     },
   ];
 
   const ipTabs = [
     {
       tabTitle: "IP Disclosures",
+      component: null,
     },
     {
       tabTitle: "Projects Deployed",
+      component: null,
     },
     {
       tabTitle: "Agreements Signed",
+      component: null,
     },
     {
       tabTitle: "IP Licensing",
+      component: null,
     },
     {
       tabTitle: "Industrial Products",
+      component: null,
     },
   ];
-
-  const [activeTab, setActiveTab] = useState("Research Project Reports");
-  const [ipActiveTab, setIpActiveTab] = useState("IP Disclosures");
-
-  const handleIpTabs = () => {
-    setIpActiveTab(!ipActiveTab);
-  };
 
   const handleTabChange = (tabTitle) => {
     setActiveTab(tabTitle);
   };
+
+  const handleIpTabs = () => {
+    setIpActiveTab(!ipActiveTab);
+    setActiveTab(ipActiveTab ? "Research Project Reports" : "IP Disclosures");
+  };
+
+  const tabs = ipActiveTab ? ipTabs : researchTabs;
 
   return (
     <div className="flex flex-col items-center justify-center gap-y-8 w-full">
       <div className="flex flex-col items-center gap-y-10 research-shadow px-4 pb-10 pt-5 rounded-[10px] bg-white w-full">
         <div className="flex items-center justify-between w-full">
           <span className="text-xl text-blue-900 font-bold">
-          {ipActiveTab
-                ? "Research Reports "
-                : "Ip and Commercialization Reports "}
+            {ipActiveTab
+              ? "IP and Commercialization Reports"
+              : "Research Reports"}
           </span>
           <div className="flex items-center">
             <button
               onClick={handleIpTabs}
               className="bg-blue-900 text-white rounded-full font-bold text-xs leading-[22px] py-1 w-40 2xl:w-56"
             >
-              {ipActiveTab
-                ? "IP and Commercialization"
-                : "Research Excellence "}
+              {ipActiveTab ? "Research Excellence" : "IP and Commercialization"}
             </button>
           </div>
         </div>
         <div className="flex flex-col lg:flex-row items-center w-full gap-x-4">
-          {!ipActiveTab
-            ? researchTabs.map((item, index) => (
-                <div
-                  key={index}
-                  className={`${
-                    activeTab === item.tabTitle
-                      ? "text-white bg-blue-900"
-                      : "bg-white text-blue-900"
-                  } cursor-pointer rounded-full tab-shadow font-bold text-base leading-[22px] px-8 py-1`}
-                  onClick={() => handleTabChange(item.tabTitle)}
-                >
-                  {item.tabTitle}
-                </div>
-              ))
-            : ipTabs.map((item, index) => (
-                <div
-                  key={index}
-                  className={`${
-                    activeTab === item.tabTitle
-                      ? "text-white bg-blue-900"
-                      : "bg-white text-blue-900"
-                  } cursor-pointer rounded-full tab-shadow font-bold text-base leading-[22px] px-8 py-1`}
-                  onClick={() => handleTabChange(item.tabTitle)}
-                >
-                  {item.tabTitle}
-                </div>
-              ))}
+          {tabs.map((item, index) => (
+            <div
+              key={index}
+              className={`${
+                activeTab === item.tabTitle
+                  ? "text-white bg-blue-900"
+                  : "bg-white text-blue-900"
+              } cursor-pointer rounded-full tab-shadow font-bold text-base leading-[22px] px-8 py-1`}
+              onClick={() => handleTabChange(item.tabTitle)}
+            >
+              {item.tabTitle}
+            </div>
+          ))}
         </div>
         <div className="w-full">
-          {researchTabs.find((item) => item.tabTitle === activeTab)?.component}
+          {tabs.find((item) => item.tabTitle === activeTab)?.component}
         </div>
       </div>
     </div>
