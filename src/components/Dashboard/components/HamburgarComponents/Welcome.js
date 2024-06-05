dbb4953db764b7fbc5691beb919008ade789091e
FahadZaheerfzr <fahadzaheer19@gmail.com>
2024-06-06 01:05:04 +0300

    Highlights added

--- src/components/Dashboard/components/HamburgarComponents/Welcome.js
+++ src/components/Dashboard/components/HamburgarComponents/Welcome.js
@@ -1,18 +1,31 @@
 import { UserContext } from '@/context/UserContext/GlobalProvider';
 import React, { useContext } from 'react'
 
-export default function Welcome() {
-  const {user} = useContext(UserContext);
+export default function Welcome({ notice }) {
+  const { user } = useContext(UserContext);
   return (
-    <div className="flex justify-between items-center w-50% bg-div-gray px-4 mt-8 rounded-lg">
+    <div>
+      <div className="flex justify-between items-center w-50% bg-red-200 p-2 mt-8 rounded-lg">
+        {notice &&
+          <div className="flex flex-col" >
+            <span className="text-xl font-bold">Highlights</span>
+            <span className="text-sm font-semibold">
+              Dear Faculty, Please submit your researches related to {notice.Thematic_Area} by {notice.Deadline}.
+              &nbsp;{notice.Eligibility_Criteria} are eligible to submit the researches. The minimum funding amount is {notice.Funding}.<br />
+              For more details, please contact {notice.Mailing_Address} or {notice.Contact_Details}.
+            </span>
+          </div>
+        }
+      </div>
+      <div className="flex justify-between items-center w-50% bg-div-gray px-4 mt-8 rounded-lg">
         {user &&
-        <div className="flex flex-col" >
-          
-          <span className="text-2xl font-bold">Hello {user.name}!</span>
-          <span className="text-xl font-semibold">{user.designation}</span>
-          
-          <span>Its good to see you again.</span>
-        </div>
+          <div className="flex flex-col" >
+
+            <span className="text-2xl font-bold">Hello {user.name}!</span>
+            <span className="text-xl font-semibold">{user.designation}</span>
+
+            <span>Its good to see you again.</span>
+          </div>
         }
         <div className="mr-4">
           <img
@@ -22,5 +35,6 @@ export default function Welcome() {
           />
         </div>
       </div>
+    </div>
   )
 }
--- src/pages/research-excellence/index.js
+++ src/pages/research-excellence/index.js
@@ -1,14 +1,33 @@
 
 import Baselayout from '@/components/FacultyDashboard/Baselayout/Baselayout'
-import ResearchExcellence  from '@/components/FacultyDashboard/ResearchExcellence/excellence'
+import ResearchExcellence from '@/components/FacultyDashboard/ResearchExcellence/excellence'
 import Welcome from '@/components/Dashboard/components/HamburgarComponents/Welcome'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 export default function index() {
+  const [notice, setNotice] = useState();
+
+  const fetchCalls = async () => {
+    try {
+      const res = await axios.get(`/api/research_calls`);
+      setNotice(res.data.calls[0]);
+    } catch (error) {
+      console.error("Error fetching notice:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCalls();
+  }, [])
+
+
+
   return (
     <Baselayout >
-    <div className='flex justify-center flex-col gap-x-6 px-8' >
-    <Welcome/>
-     <ResearchExcellence/>
-    </div>
-  </Baselayout>
+      <div className='flex justify-center flex-col gap-x-6 px-8' >
+        <Welcome notice={notice} />
+        <ResearchExcellence />
+      </div>
+    </Baselayout>
   )
 }
